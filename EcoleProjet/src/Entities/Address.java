3f0949cf82b8c4fee5a399f7fda86ced37a29ba1
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-28 11:49:53 +0200

    Class Address

--- EcoleProjet/src/Entities/Address.java
+++ EcoleProjet/src/Entities/Address.java
@@ -1,4 +1,22 @@
 package Entities;
 
 public class Address {
+    private final String street;
+    private final String city;
+    private final String postCode;
+
+    public Address(String street, String city, String postCode) {
+        this.street = street;
+        this.city = city;
+        this.postCode = postCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", postCode='" + postCode + '\'' +
+                '}';
+    }
 }
