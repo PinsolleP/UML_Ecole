7f92706d3f5225221b59f908909f500d226ef78a
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-28 09:01:51 +0200

    Constructor et getter et setter

--- EcoleProjet/src/User.java
+++ EcoleProjet/src/User.java
@@ -8,4 +8,79 @@ public abstract class User {
     String lastName;
     LocalDate dateOfBirth;
     Address address;
+
+    public User(String userName, String password, Role role, String firstName, String lastName, LocalDate dateOfBirth, Address address) {
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+    }
+
+    public User(String userName, Role role, String firstName, String lastName, LocalDate dateOfBirth, Address address) {
+        this.userName = userName;
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
