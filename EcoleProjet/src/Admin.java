98712bfd90843254fdf49134e8f04f0b374d644a
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-28 08:56:37 +0200

    Ajout des attributs de admin

--- EcoleProjet/src/Admin.java
+++ EcoleProjet/src/Admin.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+
 public class Admin extends User{
+    Admin admin;
+    Director director;
+    ArrayList<Student> students;
+    ArrayList<Teacher> teachers;
+    ArrayList<Course> courses;
 }
