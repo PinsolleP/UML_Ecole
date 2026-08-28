e2fc4b513ec6c64677f268135adfc4c0edc959fb
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-28 08:57:27 +0200

    changement Admin est school

--- EcoleProjet/src/Admin.java
+++ EcoleProjet/src/Admin.java
@@ -1,9 +1,5 @@
 import java.util.ArrayList;
 
 public class Admin extends User{
-    Admin admin;
-    Director director;
-    ArrayList<Student> students;
-    ArrayList<Teacher> teachers;
-    ArrayList<Course> courses;
+
 }
--- EcoleProjet/src/School.java
+++ EcoleProjet/src/School.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+
 public class School {
+    Admin admin;
+    Director director;
+    ArrayList<Student> students;
+    ArrayList<Teacher> teachers;
+    ArrayList<Course> courses;
 }
