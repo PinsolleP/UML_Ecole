947a33470060fc245455d114592903411b5ef0df
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-28 09:08:20 +0200

    Constructor Director

--- EcoleProjet/src/Director.java
+++ EcoleProjet/src/Director.java
@@ -1,2 +1,7 @@
+import java.time.LocalDate;
+
 public class Director extends User{
+    public Director(String userName, Role role, String firstName, String lastName, LocalDate dateOfBirth, Address address) {
+        super(userName, role, firstName, lastName, dateOfBirth, address);
+    }
 }
