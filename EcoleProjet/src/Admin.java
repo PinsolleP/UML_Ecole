65a95fcaf619ea94fa4a325dce68a0d18d587129
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-28 09:05:19 +0200

    Constructor Admin

--- EcoleProjet/src/Admin.java
+++ EcoleProjet/src/Admin.java
@@ -1,5 +1,8 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Admin extends User{
-
+    public Admin(String userName, String password, Role role, String firstName, String lastName, LocalDate dateOfBirth, Address address) {
+        super(userName, password, role, firstName, lastName, dateOfBirth, address);
+    }
 }
