86e8941adfa70752dfbc8b9e29bae0348d8dbc8c
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-28 08:49:41 +0200

    Ajout des attributs de la class User

--- EcoleProjet/src/User.java
+++ EcoleProjet/src/User.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class User {
+    String userName;
+    String password;
+    Role role;
+    String firstName;
+    String lastName;
+    LocalDate dateOfBirth;
+    Address address;
 }
