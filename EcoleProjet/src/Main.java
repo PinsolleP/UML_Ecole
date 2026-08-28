0a98dafd13f8198f800458229f95a2c6efc42d4b
Bittori Alfaro <bittori.alfaro@fms-ea.com>
2026-08-28 14:57:51 +0200

    Ajout de la consultation des élèves pour Teacher

--- EcoleProjet/src/Main.java
+++ EcoleProjet/src/Main.java
@@ -1,90 +1,113 @@
 import Entities.Admin;
 import Entities.School;
 import Entities.Session;
+import Entities.Student;
+import Entities.Teacher;
 import Entities.User;
 import utils.Input;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        Admin admin = new Admin("admin", "admin", "admin", "admin", null, null);
-        new School(admin);
-        Session session = new Session();
-        boolean sessionIsOn = true;
-        while (sessionIsOn) {
-            sessionIsOn = choice(scanner, session, random);
-        }
-        scanner.close();
-    }
+	public static void main(String[] args) {
+		Random random = new Random();
+		Scanner scanner = new Scanner(System.in);
+		Admin admin = new Admin("admin", "admin", "admin", "admin", null, null);
+		new School(admin);
+		Session session = new Session();
+		boolean sessionIsOn = true;
+		while (sessionIsOn) {
+			sessionIsOn = choice(scanner, session, random);
+		}
+		scanner.close();
+	}
 
-    public static boolean choice(Scanner scanner, Session session, Random random){
-        User user = session.getCurrentUser();
-        // Affichage des choix
-        System.out.println("Que souhaitez vous faire ?");
-        System.out.println("q- Quitter le programme");
-        if(user == null) {
-            System.out.println("1- Authentification");
-        }
-        else {
-            switch (user.getRole()) {
-                case ADMIN:
-                    System.out.println("1- Creer un nouveau Directeur");
-                    System.out.println("2- Déconnection");
-                    break;
-                case DIRECTOR:
-                    System.out.println("...");
-                    break;
-                case STUDENT:
-                    System.out.println("1- Voir mes cours");
-                    break;
-                case TEACHER:
-                    System.out.println("1- Voir mes éleves");
-                    break;
-            }
-        }
+	public static boolean choice(Scanner scanner, Session session, Random random) {
+		User user = session.getCurrentUser();
+		// Affichage des choix
+		System.out.println("Que souhaitez vous faire ?");
+		System.out.println("q- Quitter le programme");
+		if (user == null) {
+			System.out.println("1- Authentification");
+		} else {
+			switch (user.getRole()) {
+			case ADMIN:
+				System.out.println("1- Creer un nouveau Directeur");
+				System.out.println("2- Déconnection");
+				break;
+			case DIRECTOR:
+				System.out.println("...");
+				break;
+			case STUDENT:
+				System.out.println("1- Voir mes cours");
+				break;
+			case TEACHER:
+				System.out.println("1- Voir mes éleves");
+				break;
+			}
+		}
 
-        // input
-        Integer choice = Input.inputIntegerBetween(scanner, "Entrez votre choix",1, Integer.MAX_VALUE);
-        if(choice == null){
-            return false;
-        }
+		// input
+		Integer choice = Input.inputIntegerBetween(scanner, "Entrez votre choix", 1, Integer.MAX_VALUE);
+		if (choice == null) {
+			return false;
+		}
 
-        // traitement resultat
-        if(user == null) {
-            switch (choice) {
-                case 1:
-                    session.authenticate(scanner);
-                    break;
-                default:
-                    System.out.println("Choix invalide");
-                    break;
-            }
-        }
-        else {
-            switch (user.getRole()) {
-                case ADMIN:
-                    switch (choice){
-                        case 1:
-                            session.createANewDirector(scanner, random);
-                            break;
-                        case 2:
-                            System.out.println("Deconnection");
-                            session.disconnect();
-                            break;
-                    }
-                    break;
-                case DIRECTOR:
-                    break;
-                case STUDENT:
-                    break;
-                case TEACHER:
-                    break;
-            }
-        }
-        return true;
-    }
+		// traitement resultat
+		if (user == null) {
+			switch (choice) {
+			case 1:
+				session.authenticate(scanner);
+				break;
+			default:
+				System.out.println("Choix invalide");
+				break;
+			}
+		} else {
+			switch (user.getRole()) {
+			case ADMIN:
+				switch (choice) {
+				case 1:
+					session.createANewDirector(scanner, random);
+					break;
+				case 2:
+					System.out.println("Deconnection");
+					session.disconnect();
+					break;
+				}
+				break;
+			case DIRECTOR:
+				break;
+			case STUDENT:
+				break;
+			case TEACHER:
+				switch (choice) {
+				case 1:
+					Teacher teacher = (Teacher) user;
+
+					if (teacher.getCourses().isEmpty()) {
+						System.out.println("Aucun cours assigné.");
+						break;
+					}
+
+					ArrayList<Student> students = teacher.getStudents();
+
+					if (students.isEmpty()) {
+						System.out.println("Aucun élève inscrit.");
+						break;
+					}
+
+					System.out.println("Liste de vos élèves :");
+					for (Student student : students) {
+						System.out.println(student);
+					}
+					break;
+				}
+				break;
+			}
+		}
+		return true;
+	}
 }
